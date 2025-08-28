7152ab60b1ee6b9245535c9575249f24f4862744
Talha <tahaiqbal678@gmail.com>
2025-08-29 02:55:35 +0500

    changed env variable sequence

--- app/config.py
+++ app/config.py
@@ -3,9 +3,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     database_hostname: str
     database_port: str 
+    database_username: str 
     database_password: str 
     database_name: str
-    database_username: str 
     Secret_Key: str 
     Algorithm: str 
     ACCESS_TOKEN_EXPIRE_MINUTES: int
